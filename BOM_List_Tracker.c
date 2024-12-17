fcec374f61450f61ea120e0255ddcabfbb80f9ae
Kristelle <126293638+kristellesampang@users.noreply.github.com>
2024-12-18 10:14:38 +1300

    Working

--- BOM_List_Tracker.c
+++ BOM_List_Tracker.c
@@ -6,35 +6,21 @@
 // Macros
 #define MAX_LINE_LENGTH 12 // size of part number
 #define NUMBER_OF_PARTS_USED 38 // Change this for every model 
+#define UNIQUE_PARTS_SIZE 194 // Change this for every model
 
 // String input for used parts 
 char string_input[MAX_LINE_LENGTH];
-char used_parts[MAX_LINE_LENGTH][NUMBER_OF_PARTS_USED]; 
+// char used_parts[MAX_LINE_LENGTH][NUMBER_OF_PARTS_USED]; 
+char used_parts[NUMBER_OF_PARTS_USED][MAX_LINE_LENGTH]; 
 char float_input[MAX_LINE_LENGTH];
-char used_quantity[MAX_LINE_LENGTH][NUMBER_OF_PARTS_USED]; 
+// char used_quantity[MAX_LINE_LENGTH][NUMBER_OF_PARTS_USED]; 
+char used_quantity[NUMBER_OF_PARTS_USED][MAX_LINE_LENGTH]; 
 
 // All unique parts of the model, make sure this is ordered numerically from
-// smallest to largest This should match the order on the Excel sheet you have
-// created Change the size of the array accordingly
-char *all_part_numbers[86] = { 
-    "010-000-095", "010-000-096", "012-000-180", "012-000-181", "012-000-302",
-    "012-000-306", "012-000-307", "012-000-414", "012-000-416", "012-001-041",
-    "012-001-111", "012-001-133", "012-001-134", "012-001-186", "012-001-191",
-    "012-001-204", "012-001-206", "012-001-269", "012-001-271", "012-100-479",
-    "012-100-481", "012-100-482", "056-000-042", "056-000-044", "056-000-046",
-    "056-000-052", "056-000-055", "059-000-053", "059-000-058", "059-000-059",
-    "059-000-067", "101-000-469", "101-000-484", "101-000-486", "119-000-109",
-    "119-000-110", "119-000-111", "119-000-112", "201-000-092", "234-000-386",
-    "234-000-387", "234-000-481", "234-000-483", "234-001-512", "234-001-515",
-    "234-001-518", "234-001-519", "240-002-831", "240-002-836", "245-007-112",
-    "245-007-190", "245-100-018", "245-100-020", "245-100-021", "245-100-022",
-    "245-100-023", "245-100-024", "245-100-025", "245-100-026", "245-100-027",
-    "245-100-028", "245-100-029", "245-100-030", "245-100-032", "245-100-034",
-    "270-004-629", "270-004-980", "270-004-982", "270-100-333", "281-004-814",
-    "281-004-856", "281-100-006", "281-100-007", "293-000-446", "293-000-467",
-    "293-000-468", "325-000-020", "325-000-022", "325-000-024", "325-000-026",
-    "325-000-027", "325-006-002", "325-006-003", "325-066-002", "325-096-002",
-    "325-196-002"};
+// smallest to largest This should match the order on the Excel sheet you have created 
+char *all_part_numbers[UNIQUE_PARTS_SIZE] = { 
+ "010-000-096","010-000-097","010-000-098","010-000-099","010-000-100","012-000-060","012-000-302","012-000-306","012-000-307","012-000-321","012-000-322","012-000-414","012-000-416","012-001-041","012-001-111","012-001-131","012-001-132","012-001-133","012-001-134","012-001-186","012-001-191","012-001-203","012-001-204","012-001-206","012-001-269","012-001-271","012-001-275","012-100-071","012-100-479","012-100-480","012-100-481","012-100-489","012-100-499","029-000-005","029-000-064","029-000-065","029-000-367","029-000-580","029-070-001","029-143-001","029-146-001","029-147-001","029-200-003","029-200-004","029-200-005","029-200-102","029-200-131","029-206-004","053-000-029","053-000-035","053-000-042","053-000-045","053-000-048","053-000-068","053-000-073","056-000-003","056-000-043","056-000-044","056-000-045","056-000-051","056-000-054","056-000-055","056-000-167","058-000-011","059-000-021","059-000-035","059-000-053","059-000-058","101-000-479","101-000-481","101-000-482","201-000-092","234-100-015","234-100-016","234-100-017","234-100-018","234-100-019","234-100-020","234-100-021","234-100-022","234-100-023","234-100-024","234-100-025","234-100-026","234-100-027","234-100-028","234-100-029","234-100-030","234-100-031","234-100-032","234-100-033","234-100-034","234-100-035","234-100-036","234-100-037","234-100-039","234-100-040","234-100-044","234-100-045","240-002-836","240-002-844","240-002-845","240-002-846","240-100-015","240-100-019","245-100-026","245-100-031","245-100-035","245-100-036","245-100-037","245-100-038","245-100-039","245-100-040","245-100-041","245-100-042","245-100-043","245-100-044","245-100-045","245-100-047","245-100-048","245-100-049","245-100-050","245-100-051","245-100-052","245-100-053","245-100-054","245-100-055","245-100-056","245-100-057","245-100-058","245-100-059","245-100-060","245-100-061","245-100-062","245-100-063","245-100-064","245-100-065","245-100-066","245-100-067","245-100-068","245-100-069","245-100-070","246-002-453","246-002-460","246-002-466","246-100-016","246-100-018","246-100-019","246-100-021","246-100-022","246-100-023","246-100-024","246-100-025","246-100-026","246-100-027","246-100-028","246-100-029","246-100-030","281-100-008","281-100-009","281-100-010","281-100-011","281-100-012","281-100-013","281-100-014","281-100-015","281-100-016","281-100-017","281-100-018","293-000-447","293-000-468","293-000-470","293-000-472","296-100-001","296-100-002","296-100-003","296-100-005","296-100-006","296-100-007","296-100-008","296-100-009","296-100-010","307-001-102","325-126-012","325-126-013","325-236-012","325-276-012","325-316-012","325-356-012","325-436-001","325-436-002","325-456-001","325-596-001","325-636-001"
+};
 
 // Function to print ouf the quantity amount for each used part (Do not change!)
 /**
@@ -44,7 +30,7 @@ Excel, then this must be reflected on the all_parts_numbers above. This is very
 important otherwise the list cannot be directly copied and pasted onto the Excel
 sheet.
 **/
-void print_quantity(char *used_parts[], char *quantity[]) {
+void print_quantity(char parts[][MAX_LINE_LENGTH], char quantity[][MAX_LINE_LENGTH]) {
 
   // Initialise indexing variables
   int i = 0;
@@ -56,17 +42,16 @@ void print_quantity(char *used_parts[], char *quantity[]) {
   // Loop through all the parts once
   // Note: This is only done once because the list is ordered numerically.
   // This loop cannot work if the list is not in numeric order!
-  for (i = 0; i <= (sizeof(all_part_numbers) / sizeof(all_part_numbers[0]));
-       i++) {
-
-    // Print index (used for testing)
+  for (i = 0; i <= UNIQUE_PARTS_SIZE; i++){
+      // Print index (used for testing)
     // printf("Index i: %d: ", i);
+    // printf("All part numbers: %s\t", all_part_numbers[i]);
+    // printf("Part: %s\t", parts[i]);
 
     // A used part is found
-    if (!(strcmp(all_part_numbers[i], used_parts[j]))) {
-
+    if (!(strcmp(all_part_numbers[i], parts[j]))) {
       // Print out the quantity number only
-      printf("%s\n", quantity[j]);
+      printf("%s", quantity[j]);
 
       // increment the used_index
       j++;
@@ -77,6 +62,7 @@ void print_quantity(char *used_parts[], char *quantity[]) {
       // Print nothing (empty row)
       printf("\n");
     }
+
   }
 }
 
@@ -87,16 +73,17 @@ void read_string_input() {
   int i = 0;
   // Reads the user input
   while (fgets(string_input, sizeof(string_input), stdin)) {
-    if(i >= NUMBER_OF_PARTS_USED){
-      return;
-    }
-    else if(!(strcmp(string_input,"\n"))){
+    
+    if(!(strcmp(string_input,"\n"))){
       continue;
     }
     else{
       // Copy the string input into the used_parts array
       strcpy(&used_parts[i], string_input);
       i++;
+      if(i >= NUMBER_OF_PARTS_USED){
+        return;
+      }
     }
   }
   return;
@@ -134,7 +121,7 @@ int main(void) {
   //   printf("Used Parts: %s\t", used_parts[i]);
   //   printf("Used Quantity: %s\n", used_quantity[i]);
   // }
-  // print_quantity(used_parts, used_quantity);
+  print_quantity(used_parts, used_quantity);
 
   return 0;
 }
